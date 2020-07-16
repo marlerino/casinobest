490ade99aebfaf867a495b633ce964e2fbb75c30
loveisphp <pzulfugarov@yahoo.com>
2020-07-16 18:19:21 +0500

    The casinos page

--- wp-content/themes/mercury/header.php
+++ wp-content/themes/mercury/header.php
@@ -1,34 +1,40 @@
 <?php ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" id="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
-	<?php wp_head(); ?>
+    <meta charset="<?php bloginfo('charset'); ?>"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" id="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no"/>
+    <?php wp_head(); ?>
 </head>
 <body ontouchstart <?php body_class(); ?>>
-<div class="space-box relative<?php if( get_theme_mod('mercury_boxed_layout') ) { ?> enabled<?php } ?>">
+<div class="space-box <?php if (get_permalink() != "https://casinobest.mini/casinos/"): ?>relative<?php endif; ?><?php if (get_theme_mod('mercury_boxed_layout')) { ?> enabled<?php } ?>">
 
-<!-- Header Start -->
+    <!-- Header Start -->
 
-<?php
-	$header_style = get_theme_mod('mercury_header_style');
+    <?php
+    $header_style = get_theme_mod('mercury_header_style');
 
-	if ($header_style == 2) {
-		get_template_part( '/theme-parts/header/style-2' );
-	} else {
-		get_template_part( '/theme-parts/header/style-1' );
-	}
-?>
+    if ($header_style == 2) {
+        get_template_part('/theme-parts/header/style-2');
+    } else {
+        get_template_part('/theme-parts/header/style-1');
+    }
+    ?>
+    <?php if (get_permalink() == "https://casinobest.mini/casinos/"): ?>
+        <div class="top-casinos-banner">
+            <span class="ctb-title">Top 100 best online casinos in Canada</span>
+            <?= get_img_html("cards.png","ctb-cards-img") ?>
+        </div>
+    <?php endif; ?>
+    <div class="space-header-search-block fixed">
+        <div class="space-header-search-block-ins absolute">
+            <?php get_search_form(); ?>
+        </div>
+        <div class="space-close-icon desktop-search-close-button absolute">
+            <div class="to-right absolute"></div>
+            <div class="to-left absolute"></div>
+        </div>
+    </div>
 
-<div class="space-header-search-block fixed">
-	<div class="space-header-search-block-ins absolute">
-		<?php get_search_form(); ?>
-	</div>
-	<div class="space-close-icon desktop-search-close-button absolute">
-		<div class="to-right absolute"></div>
-		<div class="to-left absolute"></div>
-	</div>
-</div>
-
-<!-- Header End -->
+    <!-- Header End -->
